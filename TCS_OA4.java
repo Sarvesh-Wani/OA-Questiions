dfbf694dbb92fca745272444754ab1bd270e11e8
Sarvesh-Wani <sarveshwani7604@gmail.com>
2025-08-19 22:56:03 +0530

    initial commit

---
+++ TCS_OA4.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class TCS_OA4 {
+
+    public static int UniqueSum(int arr[])
+    {
+        int sum = 0;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for(int el : arr)
+        {
+            mp.put(el, mp.getOrDefault(el, 0)+1);
+        }
+
+        for(int key : mp.keySet())
+        {
+            if(mp.get(key) == 1)
+            {
+                sum += key;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String args[])
+    {
+        int arr[] = {1,5,3,4,1,3,2}; //11
+        System.out.println(UniqueSum(arr));
+    }
+}
